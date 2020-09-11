8114dc54cc52e0e24da69987de915ce62c289495
qinxinghua <qinxinghua@lexue.com>
2020-09-11 15:14:09 +0800

    1.项目创建 2.常量变量演示 3.打印演示

---
+++ FirstSwiftCommandLine/FirstSwiftCommandLine/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  FirstSwiftCommandLine
+//
+//  Created by qinxinghua on 2020/9/11.
+//  Copyright © 2020 qinxinghua. All rights reserved.
+//
+
+// 导包
+import Foundation
+// 打印
+//print("Hello, World!")
+
+//MARK: - 常量和变量
+let a = 5
+//a = 3
+
+var b = 3
+b = 2
+
+//MARK: - 基本打印
+print(a)
+// 字符串拼接
+print("这里演示一个拼接字符串：\(a)")
